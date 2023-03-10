fb38fa6240716e0d6d59bfe54263264d1f8cf69b
jpstephen <jpstephen174@gmail.com>
2023-03-10 20:58:35 +0530

    delivery, phone no and signature

--- RestaurantApp/RestaurantApp/Controllers/HomeController.cs
+++ RestaurantApp/RestaurantApp/Controllers/HomeController.cs
@@ -30,50 +30,90 @@ namespace RestaurantApp.Controllers
         [HttpGet]
         public ActionResult GeneratePDF(string allbills)
         {
-
-            var thebills = JsonConvert.DeserializeObject<List<Bill>>(allbills);
-            var document = new PdfDocument();
-
-
-            
-            string htmlcontent = "<table class='table-condensed' width='100%'>";
-            htmlcontent += "<tr><td colspan=5 align='right'>" + DateTime.Now.ToString("dddd, dd MMMM yyyy hh:mm tt") + "</td></tr>";
-            htmlcontent += "<tr><td colspan=5 align='center'><h2>LE LAZARO CAFE</h2>" +
-                "<br/>Hamdan Street, behind Dama's Gold" +
-                "<br/>Abu Dhabi, UAE.</td></tr>";
-            
-            htmlcontent += "<tr><td style='height:40px;border-bottom:1px solid black' colspan=5></td></tr>";
-            htmlcontent += "<tr><th>Item</th><th>Price</th><th>Quantity</th><th>Amount</th><th>VAT</th></tr>";
-            htmlcontent += "<tr><td style='height:20px;border-top:1px solid black' colspan=5></td></tr>";
-
             float totalamount = 0;
             float totalvat = 0;
+            string htmlcontent = "";
+            float finalamount = 0; 
+            //allBills? thebills = new allBills();
+            List<Bill>? allItemsadded = new List<Bill>();
 
-            foreach(var item in thebills)
+            var thebills = JsonConvert.DeserializeObject<allBills>(allbills);
+
+            if (thebills != null)
             {
-                string vat = ((5f / 100f) * item.total).ToString("0.00");
-                float amountminusvat = item.total - float.Parse(vat);
-
-                htmlcontent += "<tr><td align='center'>" + item.name + "</td>";
-                htmlcontent += "<td align='center'>" + item.price + "</td>";
-                htmlcontent += "<td align='center'>" + item.quantity + "</td>";
-                htmlcontent += "<td align='center'>" + amountminusvat + "</td>";
-                htmlcontent += "<td align='center'>" + vat + "</td></tr>";
-
-                totalamount += amountminusvat;
-                totalvat += float.Parse(vat);
+                if(thebills.itemsadded != null)
+                {
+                    allItemsadded = JsonConvert.DeserializeObject<List<Bill>>(thebills.itemsadded);
+                }
             }
 
-            float finalamount = totalamount + totalvat;
+            var document = new PdfDocument();
+
+            
+
+            
+            htmlcontent += "<table class='table-condensed'>";
+            htmlcontent += "<tr><td colspan=5 align='right'>" + DateTime.Now.ToString("dddd, dd MMMM yyyy hh:mm tt") + "</td></tr>";
+            htmlcontent += "<tr><td colspan=5 align='center'><p style='font-size:20px;'><strong>LE LAZARO CAFE</strong></p></td></tr>";
+            htmlcontent += "<tr><td colspan=5 align='center'>+971 50 780 1413" +
+                            "<br/>02 633 7336" +
+                            "<br/>Hamdan Street, behind Dama's Gold" +
+                            "<br/>Abu Dhabi, UAE.</td></tr>";
+            
+            htmlcontent += "<tr><td style='height:40px;border-bottom:1px solid black' colspan=5 align='center'><h3>TAX INVOICE</h3></td></tr>";
+            htmlcontent += "<tr><th width='35%'>Item</th><th width='15%'>Price</th><th width='15%'>Quantity</th><th width='15%'>VAT</th><th width='20%'>Amount</th></tr>";
+            htmlcontent += "<tr><td style='height:20px;border-top:1px solid black' colspan=5></td></tr>";
+
+            if(allItemsadded != null)
+            {
+                foreach (var item in allItemsadded)
+                {
+                    string vat = ((5f / 100f) * item.total).ToString("0.00");
+                    float amountminusvat = item.total - float.Parse(vat);
+
+                    htmlcontent += "<tr><td align='center'>" + item.name + "</td>";
+                    htmlcontent += "<td align='center'>" + item.price + "</td>";
+                    htmlcontent += "<td align='center'>" + item.quantity + "</td>";
+                    htmlcontent += "<td align='center'>" + vat + "</td>";
+                    htmlcontent += "<td align='center'>" + amountminusvat + "</td></tr>";
+
+                    totalamount += amountminusvat;
+                    totalvat += float.Parse(vat);
+                }
+            }
+
+            
+
+            finalamount = totalamount + totalvat;
 
             htmlcontent += "<tr><td style='height:20px;'></td><td></td><td></td><td></td><td></td></tr>";
             htmlcontent += "<tr><td style='height:20px;border-top:1px solid black;' colspan=5></td></tr>";
 
-            htmlcontent += "<tr><td></td><td></td><td align='center'></td><td align='center'>" + totalamount.ToString("0.00") + "</td><td>"+totalvat.ToString("0.00")+"</td></tr>";
-            htmlcontent += "<tr><td style='height:20px;' colspan=5></td></tr>";
-            htmlcontent += "<tr><td align='center' colspan=5>Total : "+ finalamount.ToString("0.00") +"</td></tr>";
+            htmlcontent += "<tr><td></td><td></td><td align='center'></td><td align='center'>" + totalvat.ToString("0.00") + "</td><td>"+totalamount.ToString("0.00")+"</td></tr>";
+
+            htmlcontent += "<tr><td align='center' colspan=5 style='font-size:20px;font-weight:500;'>Total : " + finalamount.ToString("0.00") +"</td></tr>";
+            htmlcontent += "<tr><td style='height:5px;' colspan=5></td></tr>";
+
+            if (thebills != null)
+            {
+                if (thebills.addDeliveryInBill != "" && thebills.addDeliveryInBill != null)
+                {
+                    htmlcontent += "<tr><td align='center'>Delivery to : </td><td colspan=4 align='left'>" + thebills.addDeliveryInBill + "</td></tr>";
+                }
+                if (thebills.addMobileNo != "" && thebills.addMobileNo != null)
+                {
+                    htmlcontent += "<tr><td align='center'>Mobile no. : </td><td colspan=4 align='left'>" + thebills.addMobileNo + "</td></tr>";
+                }
+                if (thebills.addSignatureInBill != "" && thebills.addSignatureInBill != null)
+                {
+                    htmlcontent += "<tr><td style='height:40px;' colspan=5></td></tr>";
+
+                    htmlcontent += "<tr><td align='center'>SIGNATURE</td></tr>";
+                }
+            }
+
             htmlcontent += "<tr><td style='height:30px;' colspan=5></td></tr>";
-            htmlcontent += "<tr><td colspan=5 align='center'>THANK YOU!!    VISIT AGAIN!!!</td></tr>";
+            htmlcontent += "<tr><td colspan=5 align='center'>THANK YOU!! &nbsp;&nbsp;&nbsp; VISIT AGAIN!!!</td></tr>";
 
             htmlcontent += "</table>";
 
@@ -81,7 +121,7 @@ namespace RestaurantApp.Controllers
 
             var config = new PdfGenerateConfig();
             config.PageOrientation = PageOrientation.Landscape;
-            config.ManualPageSize = new PdfSharpCore.Drawing.XSize(500, 297);
+            config.ManualPageSize = new PdfSharpCore.Drawing.XSize(600, 297);
 
             PdfGenerator.AddPdfPages(document, htmlcontent, config);
             byte[]? response = null;
@@ -108,11 +148,20 @@ namespace RestaurantApp.Controllers
     }
 
 
+    public class allBills
+    {
+        public string? addSignatureInBill { get; set; }
+
+        public string? addDeliveryInBill { get; set; }
+
+        public string? addMobileNo { get; set; }
+
+        public string? itemsadded { get; set; }
+    }
 
     public class Bill
     {
-
-        public string name { get; set; }
+        public string? name { get; set; }
         public float price { get; set; }
         public float quantity { get; set; }
         public float total { get; set; }
